76b317dfb6cac3e8e0ae1d7df3b7a063abdec0b8
atvc0202 <atulvc2001@gmail.com>
2023-03-31 23:00:06 +0530

    reformatted the code

--- home/src/test.jsx
+++ home/src/test.jsx
@@ -20,7 +20,7 @@ export default function Test() {
 
     return (
         <div className="p-5 bg-red-500 text-white -text-3xl font-bold">
-            Hello there <button onClick={getJoke}>the joke button</button>
+            Hello there <button onClick={getJoke}>the button</button>
         </div>
     )
 };
