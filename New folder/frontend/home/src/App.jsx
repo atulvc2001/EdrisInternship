0440da7c5feca947f6f747ce3a7f665155bfcc05
atvc0202 <atulvc2001@gmail.com>
2023-04-05 00:04:33 +0530

    updated the route structure of files

---
+++ New folder/frontend/home/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./header";
+import Footer from "./footer";
+import Test from "./test";
+import "./index.scss";
+import Axios from "axios";
+
+
+const App = () => (
+  <div className="text-3xl mx-auto max-w-6xl">
+    <Header />
+    <div className="my-10">
+      Home page Content
+    </div>
+    <Test />
+    <Footer />
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
