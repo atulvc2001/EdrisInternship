dc66d2380e5efef02e1213eb7b0a637152defba0
atulvc2001 <atulvc2001@gmail.com>
2023-04-06 01:57:33 +0530

    Updated the file structure

--- New folder/frontend/home/src/header.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-export default function Header() {
-    return (
-        <div className="p-5 bg-blue-500 text-white -text-3xl font-bold">
-            Micro Frontend Header
-        </div>
-    )
-}
