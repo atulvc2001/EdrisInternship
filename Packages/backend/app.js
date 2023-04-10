bc159b911a8a7f8fe713471db13aa274da130a97
atulvc2001 <atulvc2001@gmail.com>
2023-04-10 20:23:59 +0530

    feat: added backend for recieving api calls and processing them

---
+++ Packages/backend/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const axios = require('axios');
+
+const app = express();
+
+app.use('/api', createProxyMiddleware({ 
+  target: 'https://api.ng-erp.in/unicorn/dev',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api': '/enquiries/UC-211217-0005'
+  },
+  headers: {
+    'x-api-key': 'Onj86mVQAGXsbzvXEq487G1fM8wVe1G65mHsclL8'
+  }
+}));
+
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.get('http://localhost:3000/api');
+    const data = response.data;
+    console.log(data); // log the data to the console
+    res.send(data); // send a response to the client with the data
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching data from API');
+  }
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
