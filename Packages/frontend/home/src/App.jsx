cd15e26f79fccf887eb1acb94e616b9a2e3969f7
atulvc2001 <atulvc2001@gmail.com>
2023-04-11 00:03:37 +0530

    feat: switched from tailwind to css, styled the header

--- Packages/frontend/home/src/App.jsx
+++ Packages/frontend/home/src/App.jsx
@@ -2,20 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Header from "./header";
 import Footer from "./footer";
-import Test from "./test";
 import Api from "./api";
 import "./index.scss";
 import Axios from "axios";
 
 
 const App = () => (
-  <div className="text-3xl mx-auto max-w-6xl">
+  <div className="container">
     <Header />
-    <div className="my-10">
+    <div className="body">
       Home page Content
     </div>
     <Api />
-    <Test />
     <Footer />
   </div>
 );
